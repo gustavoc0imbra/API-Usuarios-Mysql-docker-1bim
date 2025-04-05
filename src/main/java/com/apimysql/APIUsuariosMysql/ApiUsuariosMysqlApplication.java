4aff7fa88b934f6a8b2097e689b1d93d97d2e28c
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-05 12:27:04 -0300

    Init project

---
+++ src/main/java/com/apimysql/APIUsuariosMysql/ApiUsuariosMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.apimysql.APIUsuariosMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuariosMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuariosMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apimysql/APIUsuariosMysql/ApiUsuariosMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimysql.APIUsuariosMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
